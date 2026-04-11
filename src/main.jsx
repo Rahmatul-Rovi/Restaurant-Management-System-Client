01888dd6fdeb90bae15ac3405cd04c5ebdc7b060
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-04-11 10:39:07 +0600

    TanStackQuery Use

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Routes'
 import AuthProvider from './providers/AuthProvider'
 import CartProvider from './providers/CartProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <CartProvider>
-        <RouterProvider router={router} />
-      </CartProvider>
+    <QueryClientProvider client={queryClient}> 
+        <CartProvider>
+          <RouterProvider router={router} />
+        </CartProvider>
+        </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
