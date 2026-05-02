76ec1fe72249bd5269912597f1506330e63d16bd
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-05-02 10:12:24 +0600

    Stats Section Updated

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -9,7 +9,7 @@ const Stats = () => {
 
     return (
         <div className="my-24 py-16 px-10 bg-slate-50/50 rounded-[3.5rem] border border-slate-100 shadow-sm relative overflow-hidden">
-            {/* ডেকোরেশনের জন্য হালকা অরেঞ্জ আভা */}
+        
             <div className="absolute -top-10 -right-10 w-40 h-40 bg-orange-100/50 rounded-full blur-3xl -z-10"></div>
             
             <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-10">
@@ -18,19 +18,19 @@ const Stats = () => {
                         key={s.id} 
                         className={`flex flex-col items-center text-center p-8 rounded-[2.5rem] transition-all duration-500 hover:bg-white hover:shadow-xl group`}
                     >
-                        {/* আইকন বক্স - হালকা অরেঞ্জ শেড */}
+                        {/* Icon Box Half orange shafe */}
                         <div className="mb-6 w-20 h-20 rounded-3xl bg-orange-100/30 flex items-center justify-center group-hover:bg-[#ff6b08] transition-all duration-500">
                             <span className="group-hover:text-white transition-colors">
                                 {s.icon}
                             </span>
                         </div>
                         
-                        {/* কাউন্ট - ডার্ক স্লেট */}
+                        {/* count dark slote */}
                         <h2 className="text-5xl font-black text-slate-800 mb-2 tracking-tighter">
                             {s.count}
                         </h2>
                         
-                        {/* লেবেল - টেক্সট কালারটা একটু সফট */}
+                        {/* Lebel- Text color */}
                         <p className="text-slate-500 font-bold uppercase tracking-[0.2em] text-[10px]">
                             {s.label}
                         </p>
